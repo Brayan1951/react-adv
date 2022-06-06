86c036c7ca336557a10142d9d7afe5598d460a2f
Brayan1951 <brayanc1951@gmail.com>
2022-06-06 17:48:25 -0400

    controil props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -14,33 +14,23 @@ export const useShoppingCart = () => {
         product: Product;
     }) => {
 
+
         setShoppingCart((oldShoppingCart) => {
 
 
-            const producInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 }
 
-            if (Math.max(producInCart.count + count, 0) > 0) {
-                producInCart.count += count
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: producInCart
-                }
+
+
+            if (count === 0) {
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+                // return { ...rest };
+                return rest;
             }
-            // boorar el producto
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return { ...rest };
 
-
-            // if (count === 0) {
-            //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            //   // return { ...rest };
-            //   return rest;
-            // }
-
-            // return {
-            //   ...oldShoppingCart,
-            //   [product.id]: { ...product, count },
-            // };
+            return {
+                ...oldShoppingCart,
+                [product.id]: { ...product, count },
+            };
         });
 
 
